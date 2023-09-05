11ba583b7dd507481b41dfceb151cbf5ff5fc606
victor braz <victor.90braz@gmail.com>
2023-09-05 22:38:27 +0200

    Install dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
+
+afterEach(cleanup);
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders App component with logo', () => {
+  render(<App />);
+  const logoImage = screen.getByAltText('logo');
+  expect(logoImage).toBeInTheDocument();
 });
