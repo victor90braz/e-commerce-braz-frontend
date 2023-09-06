70cd20165b009717ec779beb27f787c7a7e3a616
victor braz <victor.90braz@gmail.com>
2023-09-06 11:37:48 +0200

    Install style components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import "./App.css";
+import { StyledApp } from "./AppStyles";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>e-commerce braz</h1>
-      </header>
-    </div>
+    <StyledApp>
+      <div className="App">
+        <header className="App-header">
+          <h1>e-commerce braz</h1>
+        </header>
+        <main>
+          <h2>list of products</h2>
+          <ul>
+            <li>brand: samsung</li>
+            <li>price: 400€</li>
+            <li>storage: 2gb</li>
+          </ul>
+        </main>
+      </div>
+    </StyledApp>
   );
 };
 
---
+++ src/AppStyles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledApp = styled.div`
+  h1 {
+    color: red;
+  }
+`;
